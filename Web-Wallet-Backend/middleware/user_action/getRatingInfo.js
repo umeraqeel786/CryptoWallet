b52d3e66f9bac29e3935e9e5c774ae13cb78060b
umeraqeel786 <umeraqeel2017@gmail.com>
2024-04-03 19:47:03 +0500

    userRating added

--- Web-Wallet-Backend/middleware/user_action/getRatingInfo.js
+++ Web-Wallet-Backend/middleware/user_action/getRatingInfo.js
@@ -5,5 +5,14 @@ module.exports = async (product,newStar) => {
     //     return res.status(404).json({ error: 'Product not found' })
     // }
     let stars = await Review.find({ product: product._id }).select('star');
-  
+    let fiveStars = 0, fourStars = 0, threeStars = 0, twoStars = 0, oneStars = 0;
+    stars.forEach(s => {
+        if (s.star === 5) fiveStars += 1
+        if (s.star === 4) fourStars += 1
+        if (s.star === 3) threeStars += 1
+        if (s.star === 2) twoStars += 1
+        if (s.star === 1) oneStars += 1
+    })
+    //this condition is executed during postReview and editReview
+   
 }
