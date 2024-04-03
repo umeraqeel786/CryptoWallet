34d33f4ad04306ec68f8edc7db38e817d3848d3e
umeraqeel786 <umeraqeel2017@gmail.com>
2024-04-03 15:22:40 +0500

    validateUpdateDispatcher added

--- Web-Wallet-Backend/middleware/validator/index.js
+++ Web-Wallet-Backend/middleware/validator/index.js
@@ -101,3 +101,44 @@ const validatedispatcher  = req => {
     req.check("address", "Address is required").notEmpty()
     req.check("phone", "Phone is required").notEmpty()
 }
+
+
+exports.validateDispatcher = (req,res, next) => {
+    validatedispatcher(req)
+    // check for password
+    req.check("password", "Password is required").notEmpty();
+    req.check("password")
+        .isLength({ min: 6 })
+        .withMessage("Password must contain at least 6 characters")
+        .matches(/\d/)
+        .withMessage("Password must contain a number");
+    // check for errors
+    const errors = req.validationErrors();
+    // if error show the first one as they happen
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+    // proceed to next middleware
+    next();
+}
+
+exports.validateUpdateDispatcher = (req, res, next) => {
+    validatedispatcher(req)
+    // check for password
+    req.newPassword && req.check("newPassword")
+        .isLength({ min: 6 })
+        .withMessage("Password must be at least 6 chars long")
+        .matches(/\d/)
+        .withMessage("must contain a number")
+        .withMessage("Password must contain a number");
+    // check for errors
+    const errors = req.validationErrors();
+    // if error show the first one as they happen
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+    // proceed to next middleware
+    next();
+}
