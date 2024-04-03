73d6bd89f9dc643b53f6604968f66dcccb700430
umeraqeel786 <umeraqeel2017@gmail.com>
2024-04-03 19:49:54 +0500

    userRev added

---
+++ Web-Wallet-Backend/middleware/user_action/userHas.js
@@ -0,0 +1,12 @@
+const Cart = require("../../models/Cart")
+const Review = require("../../models/Review")
+const Order = require("../../models/Order")
+const Whislist = require("../../models/WishList")
+
+module.exports = async(product,user,type) =>{
+    let hasOnCart = null
+    let hasBought = null
+    let hasOnWishlist = null
+    let hasReviewed = null
+  
+}
