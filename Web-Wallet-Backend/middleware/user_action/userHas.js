c016215d8583f96d87c2ba538a46a149267e9151
umeraqeel786 <umeraqeel2017@gmail.com>
2024-04-03 19:51:05 +0500

    userRev added

--- Web-Wallet-Backend/middleware/user_action/userHas.js
+++ Web-Wallet-Backend/middleware/user_action/userHas.js
@@ -27,7 +27,9 @@ module.exports = async(product,user,type) =>{
         if (type==='product') {
             //has bought?
             hasBought = await Order.findOne({ user: user, $or: [{ 'status.currentStatus': 'complete' }, { 'status.currentStatus': 'tobereturned', 'status.currentStatus': 'return' }] })
-          
+            hasBought ? hasBought = true : hasBought = false
+            
+            
         }
     }
     
